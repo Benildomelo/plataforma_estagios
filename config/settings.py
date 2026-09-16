831a91e37f9f14e1372897e8e99e8e5633abdc88
Benildo <benildomelo2@gmail.com>
2026-09-15 21:30:39 -0300

    Preparar projeto para deploy

--- config/settings.py
+++ config/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "vagas" / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "vagas" / "static",
-]
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
